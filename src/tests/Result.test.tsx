2e983cd95beaa384cb75025b5421e14d5ffb2072
Alexandre Ribeiro da Silva <ardsilva87@gmail.com>
2020-06-01 09:26:09 -0300

    Delete Result.test.tsx

--- src/tests/Result.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Result from '../containers/Result';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Result />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
