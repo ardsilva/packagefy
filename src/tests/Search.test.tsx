78ee071e0313b07680a644722a79190d47625fb5
Alexandre Ribeiro da Silva <ardsilva87@gmail.com>
2020-06-01 09:26:17 -0300

    Delete Search.test.tsx

--- src/tests/Search.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Search from '../containers/Search';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Search showGraphics={true} mountPackage={[]} />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
